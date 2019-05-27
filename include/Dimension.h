e220e882786a6ca6197f87e47f7721d3c72fd2b6
panaka_13 <nhatkhang589@gmail.com>
2019-05-27 14:27:48 -0500

    clang-format

--- include/Dimension.h
+++ include/Dimension.h
@@ -3,15 +3,16 @@
 #include <vector>
 
 class Dimension : protected std::vector<unsigned int> {
-  public:
-    Dimension() : std::vector<unsigned int>() {}
-    Dimension(std::initializer_list<unsigned int> value) : std::vector<unsigned int>(value) {}
+public:
+  Dimension() : std::vector<unsigned int>() {}
+  Dimension(std::initializer_list<unsigned int> value)
+      : std::vector<unsigned int>(value) {}
 };
 
 class Dimension2D : protected Dimension {
-  public:
-    unsigned int getRow() const { return this->at(0); }
-    unsigned int getColumn() const { return this->at(1); }
-    Dimension2D() : Dimension() {}
-    Dimension2D(std::initializer_list<unsigned int> value) : Dimension(value) {}
+public:
+  unsigned int getRow() const { return this->at(0); }
+  unsigned int getColumn() const { return this->at(1); }
+  Dimension2D() : Dimension() {}
+  Dimension2D(std::initializer_list<unsigned int> value) : Dimension(value) {}
 };
